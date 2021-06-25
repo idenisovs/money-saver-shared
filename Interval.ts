2c97ff0f9ae70149ced143265e433d9847ce18f0
Ilya Denisov <i.denisovs@gmail.com>
2021-06-25 20:42:41 +0300

    Interval object got init for Start and End dates.

--- Interval.ts
+++ Interval.ts
@@ -1,13 +1,16 @@
 export class Interval {
-  id?: number;
-  start = new Date();
-  end = new Date();
-  sum = 0;
-  latest? = false;
+	id?: number;
+	start = new Date();
+	end = new Date();
+	sum = 0;
+	latest? = false;
 
-  constructor(dto?: Interval) {
-    if (dto) {
-      Object.assign(this, dto);
-    }
-  }
+	constructor(dto?: Interval) {
+		if (dto) {
+			Object.assign(this, dto);
+
+			this.start = new Date(dto.start);
+			this.end = new Date(dto.end);
+		}
+	}
 }
