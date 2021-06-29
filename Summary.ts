891ffdc2e54ab9bc7ee4c03a7571379c9c8fc09f
Ilya Denisov <i.denisovs@gmail.com>
2021-06-29 13:42:47 +0300

    Structure of Summary object is changed, so interval now is separate field.

--- Summary.ts
+++ Summary.ts
@@ -2,27 +2,16 @@ import { Interval } from './Interval';
 import { Day } from './Day';
 import { Totals } from './Totals';
 
-export class Summary extends Interval {
+export class Summary {
+	interval = new Interval();
 	days: Day[] = [];
 	totals = new Totals();
 
 	constructor(dto?: Summary) {
-		super(dto);
-
 		if (dto) {
+			this.interval = new Interval(dto.interval);
 			this.days = dto.days.map<Day>(d => new Day(d));
 			this.totals = dto.totals;
 		}
-	}
-
-	static fromInterval(interval: Interval): Summary {
-		const summary = new Summary();
-
-		summary.start = interval.start;
-		summary.end = interval.end;
-		summary.sum = interval.sum;
-		summary.latest = interval.latest;
-
-		return summary;
 	}
 }
