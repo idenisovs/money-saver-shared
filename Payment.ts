ebf884b6fc36e33e0adca4c6c92f4110eff21118
Ilja Denisovs <i.denisovs@gmail.com>
2024-10-25 13:22:12 +0300

    TST-8: Payment DTO got the Created At field.

--- Payment.ts
+++ Payment.ts
@@ -1,15 +1,11 @@
 export class Payment {
 	id?: number;
-
 	date: string = '';
-
 	sum = 0;
-
 	add?: boolean;
-
 	update?: boolean;
-
 	remove?: boolean;
+	createdAt = new Date();
 
 	constructor(dto?: Payment) {
 		if (!dto) {
@@ -17,5 +13,9 @@ export class Payment {
 		}
 
 		Object.assign(this, dto);
+
+		if (dto.createdAt) {
+			this.createdAt = new Date(dto.createdAt);
+		}
 	}
 }
